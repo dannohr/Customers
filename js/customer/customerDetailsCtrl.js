a8e4dbb43c9ee3520961457c44b81b8cfdfa6ead
dannohr <danielnohr@gmail.com>
2017-09-15 10:49:21 -0500

    commit before changing customer edit page

--- js/customer/customerDetailsCtrl.js
+++ js/customer/customerDetailsCtrl.js
@@ -6,18 +6,20 @@ angular.module('app').controller('custDetailsCtrl', function ($scope, $statePara
         customerSrvc.getCustomerDetail(id).then (function(response) {
           $scope.customersDetails = response.data[0];
           console.log(response.data[0])
-          console.log(response.data[0]["main_location[town]"])
+          $scope.address = response.data[0]["main_location[street_address]"];
           $scope.city = response.data[0]["main_location[town]"];
+          $scope.state = response.data[0]["main_location[county_province_state]"];
+          $scope.zip = response.data[0]["main_location[postcode]"];
+          $scope.phone = response.data[0]["main_location[telephone]"];
     
         })
       }
       
       
 
-
-
       $scope.getCustomersDetails($stateParams.id)
   
+
 
 
 
@@ -49,6 +51,32 @@ angular.module('app').controller('custDetailsCtrl', function ($scope, $statePara
         })
       }
        
+
+
+      $scope.save = function(id) {
+              
+        if(typeof id === 'undefined') {  //Will be undefined if there was not an existing customer to edit
+          customerSrvc.addNewCustomer($scope.customersDetails.name).then (function(response) {
+            $scope.newCustomer = response;
+            console.log(response)
+            console.log(response.data.data.id)
+            $scope.getCustomersDetails(response.data.data.id)
+          })
+
+
+
+        } else {
+          console.log("Update Existing Customer")
+          customerSrvc.updateCustomer(id, $scope.customersDetails.lock_version, $scope.customersDetails.name).then (function(response) {
+            $scope.customerUpdated = response;
+            console.log(response)
+            $scope.getCustomersDetails(response.id)
+      
+          })
+        }
+
+     }
+
      
       
   })
